c989e0096bb9f0addf503d188fa2c38981c08c74
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-04 23:41:35 +0900

    백준 문제풀이
    쉬운내용이라 메모 따로 안함

---
+++ bj_10950.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class bj_10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		int a,b;
+		for(int i = 0; i < count; i++) {
+			a =sc.nextInt();
+			b =sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
