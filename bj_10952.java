c2a2bf4fab4eb9b9b74fcb8208a76f2093175015
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-05 23:59:20 +0900

    백준 문제풀이
    while 반복문

---
+++ bj_10952.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bj_10952 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a*b == 0) {
+				break;
+			}
+			System.out.println(a+b);
+		}
+
+	}
+
+}
