1a676a25faa1206908daea4092d6c29793436214
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-06 01:28:38 +0900

    백준 문제풀이
    배열 / 해당 수열에 각 숫자의 개수가 몇번 나오는지 맞추는 문제, 인수를 문자열로 바꾸는 방법에 대해서 학습, 더 간단히 할 수 있을거 같음

--- bj_1110.java
+++ bj_1110.java
@@ -4,11 +4,11 @@ public class bj_1110 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int first = sc.nextInt();
-		int last = first;
-		int count = 0;
+		int first = sc.nextInt();//초기값
+		int last = first;//초기 1회 반복을 위한 동일화
+		int count = 0;//카운트
 		
-		do{
+		do{ 	//조건문이 first != last임으로 한번은 실행
 			int ten = last/10;
 			int one = last%10;
 			last  = (ten+one)%10 + one*10;
