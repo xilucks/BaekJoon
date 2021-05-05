51182d05bd47ef154e3f7d4530252e06f5ea8de9
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-06 00:13:32 +0900

    백준 문제풀이
    while 반복문 더하기 사이클

--- bj_1110.java
+++ bj_1110.java
@@ -1,8 +1,20 @@
-
+import java.util.Scanner;
 public class bj_1110 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int first = sc.nextInt();
+		int last = first;
+		int count = 0;
+		
+		do{
+			int ten = last/10;
+			int one = last%10;
+			last  = (ten+one)%10 + one*10;
+			count ++;
+		}while(first != last); 
+		System.out.println(count);
 
 	}
 
