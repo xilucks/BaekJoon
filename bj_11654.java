eb7252029b7beceeaa6675fc0743d14fa40a795f
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-08 23:39:26 +0900

    백준문제풀이
    문자열

---
+++ bj_11654.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class bj_11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String num = sc.next();
+		char num2 = num.charAt(0);
+		int num3 = (int)num2;
+		System.out.println(num3);
+
+	}
+
+}
