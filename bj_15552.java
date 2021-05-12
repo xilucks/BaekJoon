3c9b210bdd1c2077d472bccb89602b3c307a70e8
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-12 20:47:40 +0900

    백준문제풀이

--- bj_15552.java
+++ bj_15552.java
@@ -5,9 +5,18 @@ public class bj_15552 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a,b;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		System.out.println(br.readLine());
+		
+		for(int i = 0 ; i <n; i ++) {
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		
+		}
+		
 		br.close();
 	}
 
