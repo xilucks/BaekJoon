260af7da6a3e7d8c997b3bf00a8f1afc2da17aa9
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-21 00:02:55 +0900

    백준문제풀이/문자열
    빠른 A+B
    br bw사용법 숙달

--- bj_15552.java
+++ bj_15552.java
@@ -1,23 +1,26 @@
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 public class bj_15552 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int a,b;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    
+		int n = Integer.parseInt(br.readLine());
 		for(int i = 0 ; i <n; i ++) {
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		bw.write((int)(a+b)+"\n");
 		
 		}
-		
-		br.close();
+		bw.flush();
 	}
 
 }
