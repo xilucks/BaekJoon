2f5021fd2927fc0f1a3d755b413a97fc411616b5
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-25 00:23:26 +0900

    백준문제풀이/큐
    출력속도개선/ 시간초과 ->정답

--- bj_18258.java
+++ bj_18258.java
@@ -1,50 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class bj_18258 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int[] queue = new int[2000000];
-		int num = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num = Integer.parseInt(st.nextToken());
 		int Front = 0;
 		int Rear = 0;
 		for(int i = 0; i<num ; i++) {
-		String str = sc.next();
+		st = new StringTokenizer(br.readLine());
+		String str = st.nextToken();
 		switch(str) {
 		case "push" :
-			int a = sc.nextInt();
+			int a = Integer.parseInt(st.nextToken());
 			queue[Rear] = a;
 			Rear ++;
 			break;
 		case "pop" :
-			if(Rear == Front) sb.append(-1+"\n");
-			else {sb.append(queue[Front]+"\n");
+			if(Rear == Front) bw.write(-1+"\n");
+			else {bw.write(queue[Front]+"\n");
 			Front++;
 			}
 			break;
 		case "size" :
 			int size = Rear - Front;
-			sb.append(size+"\n");
+			bw.write(size+"\n");
 			break;
 		case "empty" :
 			if(Rear == Front) {
-				sb.append("1"+"\n");
+				bw.write("1"+"\n");
 			}
-			else sb.append("0"+"\n");
+			else bw.write("0"+"\n");
 			break;
 		case "front" :
-			if(Rear == Front) sb.append(-1+"\n");
-			else sb.append(queue[Front]+"\n");
+			if(Rear == Front) bw.write(-1+"\n");
+			else bw.write(queue[Front]+"\n");
 			break;
 		case "back" :
-			if(Rear == Front) sb.append(-1+"\n");
-			else sb.append(queue[Rear-1]+"\n");
+			if(Rear == Front) bw.write(-1+"\n");
+			else bw.write(queue[Rear-1]+"\n");
 			break;
 		}
 		}
-		System.out.println(sb);
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
